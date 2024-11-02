ec4965462f04555631c8dc20da3c7cf945ab213d
NikitaKorneev <nickkorneevspace@gmail.com>
2024-11-03 00:05:25 +0300

    downgrade deps and add text with drei

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,7 @@ export default [
 			"react/jsx-no-target-blank": "off",
 			"react/prop-types": "off",
 			"react/no-unknown-property": "off",
+			"react/no-children-prop": "off",
 			"react-refresh/only-export-components": [
 				"warn",
 				{ allowConstantExport: true },
